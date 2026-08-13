4e863443f0d1e76d5c49840eb1bd8d734cc7c04b
Matt Carlucci <mcarlucci@lumindigital.com>
2026-08-13 12:44:33 -0500

    feat: add fidelity validation pass for Figma and image flows in SKILL.md

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 
 @Component({
@@ -7,10 +7,31 @@ import { Router } from '@angular/router';
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
+    isBackBtnVisible = false;
+    private hideTimer: ReturnType<typeof setTimeout> | null = null;
+
     constructor(public router: Router) {}
 
     get isPrototypeRoute(): boolean {
         return this.router.url !== '/';
+    }
+
+    @HostListener('document:mousemove')
+    @HostListener('document:scroll')
+    onActivity(): void {
+        this.isBackBtnVisible = true;
+        if (this.hideTimer) {
+            clearTimeout(this.hideTimer);
+        }
+        this.hideTimer = setTimeout(() => {
+            this.isBackBtnVisible = false;
+        }, 3000);
+    }
+
+    ngOnDestroy(): void {
+        if (this.hideTimer) {
+            clearTimeout(this.hideTimer);
+        }
     }
 }
